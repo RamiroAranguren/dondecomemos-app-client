a67f89075242c16bafc7b8545b865cb968dc347d
Josue Carrasco <jos247car@gmail.com>
2020-06-16 15:05:21 -0300

    trabajando estilos de restaurantes favoritos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'step-functions',
+    path: 'home',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
