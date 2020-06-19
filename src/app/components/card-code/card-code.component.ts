6fea41bd77266ced94f0a77c5ea829a7865648b2
Josue Carrasco <jos247car@gmail.com>
2020-06-19 16:35:51 -0300

    color focus en input de  popover codigo de seguridad

--- src/app/components/card-code/card-code.component.ts
+++ src/app/components/card-code/card-code.component.ts
@@ -6,21 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-code.component.scss'],
 })
 export class CardCodeComponent implements OnInit {
-  inputFocus:boolean= false; 
 
   constructor() { }
 
   ngOnInit() {}
-
-  setFocus(){
-    this.inputFocus= true;
-    console.log("focus ")
-  }
-
-  onBlur(){
-    this.inputFocus = false;
-    console.log("blur")
-  }
-
 
 }
