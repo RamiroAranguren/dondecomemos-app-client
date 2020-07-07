4f4a95aed2a382fb426acf4321a253e207f65cb3
Josue Carrasco <jos247car@gmail.com>
2020-07-07 12:40:53 -0300

    pantalla reseña y calificacion

--- src/app/pages/rate/rate/rate.page.ts
+++ src/app/pages/rate/rate/rate.page.ts
@@ -2,21 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-rate',
-  templateUrl: './rate.page.html',
-  styleUrls: ['./rate.page.scss'],
+    selector: 'app-rate',
+    templateUrl: './rate.page.html',
+    styleUrls: ['./rate.page.scss'],
 })
 export class RatePage implements OnInit {
+    disabledButton: boolean = false;
+    constructor(
+        private navCtrl: NavController
+    ) { }
 
-  constructor(
-    private navCtrl: NavController
-  ) { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
-  goToReview() {
-    this.navCtrl.navigateForward('/order/review');
-  }
+    goToReview() {
+        this.navCtrl.navigateForward('/order/review');
+    }
 
 }
